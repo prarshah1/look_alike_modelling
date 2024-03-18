74a5b25dbf8a80d6fe26ca81010ad64dc4bf84f9
prarshahdv <prarthana.shah@dataverze.ai>
2024-03-18 09:54:43 +0530

    demo changes - landing page

--- pages/similance_credit.py
+++ pages/similance_credit.py
@@ -45,7 +45,7 @@ with st.container(border=True):
     This way, we hope to keep more customers happy and stop them from leaving us. 
     """)
 
-rows_to_convert_credit = 'CLIENTNUM,Attrition_Flag,Customer_Age,Gender,Dependent_count,Education_Level,Marital_Status,Income_Category,Card_Category,Months_on_book,Total_Relationship_Count,Months_Inactive_12_mon,Contacts_Count_12_mon,Credit_Limit,Total_Revolving_Bal,Avg_Open_To_Buy,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,Total_Trans_Ct,Total_Ct_Chng_Q4_Q1,Avg_Utilization_Ratio'.split(",")
+rows_to_convert_credit = 'Customer_Age,Gender,Dependent_count,Education_Level,Marital_Status,Income_Category,Card_Category,Months_on_book,Total_Relationship_Count,Months_Inactive_12_mon,Contacts_Count_12_mon,Credit_Limit,Total_Revolving_Bal,Avg_Open_To_Buy,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,Total_Trans_Ct,Total_Ct_Chng_Q4_Q1,Avg_Utilization_Ratio'.split(",")
 
 if 'generated_df' not in st.session_state:
     st.session_state.generated_df = None
